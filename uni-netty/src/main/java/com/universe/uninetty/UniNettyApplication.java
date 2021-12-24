3d97af1c0bb9c2bb5bdfbe2a80f2c2b8f3729063
hongqi <hqzc123>
2021-12-24 13:19:06 +0800

    pom 规范修改

---
+++ uni-netty/src/main/java/com/universe/uninetty/UniNettyApplication.java
@@ -0,0 +1,13 @@
+package com.universe.uninetty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniNettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniNettyApplication.class, args);
+    }
+
+}
---
+++ uni-rabbin/src/test/java/com/universe/rabbin/RabbinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.universe.rabbin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

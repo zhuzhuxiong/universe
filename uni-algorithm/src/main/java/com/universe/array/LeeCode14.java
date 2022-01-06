883389480d63698d974addbba25e5cf46fda255c
hongqi <hqzc123>
2022-01-06 14:17:21 +0800

    LeeCode13 罗马数字 to int

---
+++ uni-algorithm/src/main/java/com/universe/array/LeeCode14.java
@@ -0,0 +1,18 @@
+package com.universe.array;
+
+/**
+ * @author
+ */
+public class LeeCode14 {
+
+    public static void main(String[] args) {
+
+    }
+
+//    public String solution(String[] strs) {
+//        String s1 = strs[0];
+//        for (int i = 0; i < strs.length; i++) {
+//
+//        }
+//    }
+}
--- uni-vertx/src/main/java/com/universe/vertx/VertxApplication.java
+++ uni-vertx/src/main/java/com/universe/vertx/VertxApplication.java
@@ -2,13 +2,15 @@ package com.universe.vertx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class VertxApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VertxApplication.class, args);
     }
+
+
 
 }

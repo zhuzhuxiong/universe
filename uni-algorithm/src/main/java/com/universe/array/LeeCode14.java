0ce08c1d0bbbb8b36c32c25fa72c4f3adf349648
慕卿 <1119200166@qq.com>
2022-01-10 14:22:15 +0800

    vertx SimpleDemo

--- uni-algorithm/src/main/java/com/universe/array/LeeCode14.java
+++ uni-algorithm/src/main/java/com/universe/array/LeeCode14.java
@@ -6,13 +6,54 @@ package com.universe.array;
 public class LeeCode14 {
 
     public static void main(String[] args) {
-
+        String[] strs = {"aaa","aa","aaa"};
+        String solution = new LeeCode14().solution(strs);
+        System.out.println(solution);
     }
 
-//    public String solution(String[] strs) {
-//        String s1 = strs[0];
-//        for (int i = 0; i < strs.length; i++) {
-//
-//        }
-//    }
+    /**
+     * 横向对比
+     * @param strs 输入
+     * @return L
+     *
+     */
+    public String solution(String[] strs) {
+        if (null == strs || strs.length == 0){
+            return "";
+        }
+        String fir = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            String str = strs[i];
+            int min = Math.min(fir.length(), str.length());
+            int index = 0;
+            while (index < min && fir.charAt(index) == str.charAt(index)){
+                index++;
+            }
+            fir  = fir.substring(0,index);
+            if (fir.length() == 0){
+                break;
+            }
+
+        }
+        return fir;
+    }
+
+    /**
+     * 纵向对比
+     * @return L
+     */
+    public String solution2(String[] strs) {
+        if (null == strs || 0 == strs.length){
+            return "";
+        }
+        int strsLength = strs.length;
+        int length = strs[0].length();
+        for (int i = 0; i < length; i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strsLength; j++) {
+//                if ()
+
+            }
+        }
+    }
 }

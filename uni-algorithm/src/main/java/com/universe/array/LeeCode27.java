cf65db56704b848556ab444c8f70921f5bd50460
xiangLi <1119200166@qq.com>
2022-01-13 16:27:10 +0800

    LeeCode27 清楚数组重复字符 双指针

---
+++ uni-algorithm/src/main/java/com/universe/array/LeeCode27.java
@@ -0,0 +1,37 @@
+package com.universe.array;
+
+/**
+ * @author
+ */
+public class LeeCode27 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 双指针： 一 fast 一 slow；
+     * fast做循环；slow做收集；
+     *
+     * 循环条件：not for for for；
+     * while（）：left < right; fast < lenth;
+     *
+     * @param nums
+     * @return
+     */
+    public int solution(int[] nums,int val){
+
+        if (null == nums || nums.length == 0){
+            return 0;
+        }
+        int fast = 0, slow = 0;
+        while (fast < nums.length){
+            if (nums[fast] != val){
+                nums[slow] = nums[fast];
+                slow ++;
+            }
+            fast ++;
+        }
+        return slow;
+    }
+}

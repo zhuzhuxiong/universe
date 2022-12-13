df1fc9267ad2a70cd401d2ac75d900525b05e27b
xiangLi <1119200166@qq.com>
2022-12-13 14:38:09 +0800

    AI 模块 删除文件

--- uni-algorithm/src/test/java/AlgorithmTest.java
+++
@@ -1,8 +0,0 @@
-import com.universe.AlgorithmApplication;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest(classes = AlgorithmApplication.class)
-public class AlgorithmTest {
-
-
-}

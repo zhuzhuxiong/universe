40ab5a27d56a0fc5b07d84e4aaa5fcfef1a78ed6
xiangLi <1119200166@qq.com>
2022-12-13 17:02:13 +0800

    schedule 模块 修改文件

--- uni-schedule/src/main/java/com/universe/schedule/ejob/MySimpleJob.java
+++ uni-schedule/src/main/java/com/universe/schedule/ejob/MySimpleJob.java
@@ -1,8 +1,8 @@
 package com.universe.schedule.ejob;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.shardingsphere.elasticjob.api.ShardingContext;
-import org.apache.shardingsphere.elasticjob.simple.job.SimpleJob;
+//import org.apache.shardingsphere.elasticjob.api.ShardingContext;
+//import org.apache.shardingsphere.elasticjob.simple.job.SimpleJob;
 import org.springframework.stereotype.Service;
 
 //@Slf4j

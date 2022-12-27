2bccb773b43dec154dd96440f51d995a39179cea
xiangLi <1119200166@qq.com>
2022-12-27 10:57:55 +0800

    spring 模块 Aspect初试1

---
+++ uni-spring/src/main/java/com/universe/spring/aspect/AspectDemo.java
@@ -0,0 +1,61 @@
+package com.universe.spring.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+/**
+ * 切面demo
+ * 1、注解功能
+ * 2、表达式
+ * @author lx
+ */
+@Slf4j
+@Aspect
+@Component
+public class AspectDemo {
+
+    /**
+     * @Pointcut 参数：value；argNames
+     * value：
+     *  args():
+     *  @args():
+     *  target():
+     *  @target():
+     *  within():
+     *  @within():
+     *  this():
+     *  @annotation()：
+     *  **execution()：
+     *      ():(..) 表示所有参数
+     *          ()  代表一个参数
+     *     (,String) 代表第一个参数为任意参数，第二个参数为String
+     *
+     *
+     */
+    @Pointcut(value = "execution(* com.universe.spring.controller.*.*(..))")
+//    @Pointcut(argNames = "")
+//    @Pointcut(value = "@annotation()")
+//    @Pointcut(value = "execution(public * *(..))")
+    private void pointCut() {
+    }
+
+    @Before(value = "pointCut()")
+    public void beforePoint() {
+        log.info("beforePoint:切点前");
+    }
+
+    @After(value = "pointCut()")
+    public void afterPoint() {
+        log.info("afterPoint:切点后");
+    }
+
+    @Around(value = "pointCut()")
+    public Object aroundPoint(ProceedingJoinPoint pjp){
+        log.info("aroundPoint:包围你");
+        return null;
+    }
+
+
+}

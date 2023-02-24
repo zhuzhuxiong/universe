4dff38c167a69c92cc4fe57f81340ed61ad92203
xiangLi <1119200166@qq.com>
2023-02-24 09:10:19 +0800

    algorithm-lee58、66

--- uni-algorithm/src/main/java/com/universe/al/fibonacci/Fibonacci.java
+++ uni-algorithm/src/main/java/com/universe/al/fibonacci/Fibonacci.java
@@ -1,36 +1,53 @@
 package com.universe.al.fibonacci;
 
+
 /**
- *
  * 费内巴切
- * 青蛙跳台阶
- */
+ * 爬楼梯 LeeCode70
+ *  @author l
+ *  @date 2023/2/22 14:04
+*/
 public class Fibonacci {
 
+
     public static void main(String[] args) {
-        int fib = fib2(3);
-        System.out.println(fib);
     }
 
-    //动态规划
-    //设正整数 x, y, px,y,p ，求余符号为 \odot⊙ ，则有 (x + y) \odot p = (x \odot p + y \odot p) \odot p(x+y)⊙p=(x⊙p+y⊙p)⊙p 。
-    public static int fib(int n){
-        int a = 0,b = 1,num;
-        for (int i = 0; i < n; i++) {
-            num = (a + b)%1000000007;
-            a = b;
-            b = num;
-        }
-        return a;
-
-    }
-
-    //基础问题，递归版
-    public static int fib2(int n){
-        if (n == 0 || n==1){
+    /**
+     * 递归方式 会超时
+     * @author l
+     * @date  2023/2/22 14:11
+     * @param n
+     * @return int
+     **/
+    public int climbStairs(int n) {
+        if (n == 0 || n ==1) {
             return n;
         }else {
-            return fib2(n-1) + fib2(n-2);
+           return climbStairs(n-1) + climbStairs(n-2);
         }
     }
+
+    /**
+     * 动态规划 答案
+     * 本问题其实常规解法可以分成多个子问题，爬第n阶楼梯的方法数量，等于 2 部分之和
+     *
+     * 爬上 n−1 阶楼梯的方法数量。因为再爬1阶就能到第n阶
+     * 爬上 n−2 阶楼梯的方法数量，因为再爬2阶就能到第n阶
+     * f(n) = f(n-1) + f(n-2)
+     * @author l
+     * @date  2023/2/22 14:12
+     * @param n
+     * @return int
+     **/
+    public static int fib(int n){
+        int p,q = 0,r = 1;
+        for (int i = 1; i <= n; i++) {
+            p = q;
+            q = r;
+            r = p+ q;
+        }
+        return r;
+    }
+
 }
